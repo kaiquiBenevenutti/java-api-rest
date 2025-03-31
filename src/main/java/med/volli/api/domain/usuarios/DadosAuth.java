253bba7c7d405193e1eb8b7ebef13a75ac7a1b32
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-03-31 16:39:13 -0300

    adicionando metodo de login

---
+++ src/main/java/med/volli/api/domain/usuarios/DadosAuth.java
@@ -0,0 +1,4 @@
+package med.volli.api.domain.usuarios;
+
+public record DadosAuth(String login, String senha) {
+}
--- src/main/java/med/volli/api/domain/usuarios/Usuarios.java
+++ src/main/java/med/volli/api/domain/usuarios/Usuarios.java
@@ -2,6 +2,12 @@ package med.volli.api.domain.usuarios;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "Usuario")
 @Table(name = "usuarios")
@@ -10,10 +16,45 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 
-public class Usuarios {
+public class Usuarios implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String login;
     private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USUARIO"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
