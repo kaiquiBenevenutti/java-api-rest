d8fe6984e735c5266d0ba96a5b04264abe1119c0
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-03-31 17:12:21 -0300

    adicionando validacao com token JWT

--- src/main/java/med/volli/api/controllers/AuthController.java
+++ src/main/java/med/volli/api/controllers/AuthController.java
@@ -2,6 +2,9 @@ package med.volli.api.controllers;
 
 import jakarta.validation.Valid;
 import med.volli.api.domain.usuarios.DadosAuth;
+import med.volli.api.domain.usuarios.Usuarios;
+import med.volli.api.infra.security.DadosTokenJWT;
+import med.volli.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,12 +21,17 @@ public class AuthController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid DadosAuth dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var auth = manager.authenticate(token);
+        var AuthToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var auth = manager.authenticate(AuthToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.createToken((Usuarios) auth.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 
 }
